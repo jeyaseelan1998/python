63f21ffda68f05edd666ebb80ae0aaa61f72fc6b
jeyaseelan1998 <jeyaseelan.work@gmail.com>
2023-01-30 08:50:55 +0530

    working with log files

--- 3 regular exp/example_3.py
+++ 3 regular exp/example_3.py
@@ -1,8 +1,18 @@
 import re
-text = "CAT caret crt cgt cmt"
 
-# raw string with dot wildcard
-print(re.search(r"c.t", text))
-print(re.search(r"c.t", text, re.IGNORECASE))
 
-print(re.search(r"c", text))
+def show_time_of_pid(line):
+    pattern = r"([\w\s:]{14})[\s.\w=]+\[(\d+)\]"
+    result = re.search(pattern, line)
+    return (result[1]+" pid:"+result[2])
+
+
+# Jul 6 14:01:23 pid:29440
+print(
+    show_time_of_pid(
+        "Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")
+)
+
+# Jul 6 14:02:08 pid:29187
+print(show_time_of_pid(
+    "Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)"))
